a17595aa0bbdc877aed874031841a6d041ea4bb8
Rasmiranjan Nayak <nayakrasmiranjan1616@gmail.com>
2025-01-25 00:09:33 +0530

    fixing error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from './components/Navbar';
+import NavBar from './components/NavBar';
 import Welcome from './components/Welcome';
 import AboutMe from './components/AboutMe';
 const App = () => {
